7ebd71439c7a5418a09b7254f66abe6b9a9ee7a7
@bestjobs <bestjobsgithub@gmail.com>
2026-08-26 02:20:32 +0300

    Update db_sofia.js

--- db_sofia.js
+++ db_sofia.js
@@ -1,15 +1,17 @@
 var quizDatabase = {
     "biology": [
         {
-            "t": "Съединителна тъкан",
-            "q": "Коя от посочените разновидности НЕ е вид съединителна тъкан?",
+            "salt": "YmI0YmI0YmI0YmI0YmI0Yg==",
+            "iv": "YmI0YmI0YmI0YmI0",
+            "t": "9O8b1m88vO7Ebxs=",
+            "q": "r7cfXv9fXt9W32P/v+O99uV4XvfS2w==",
             "k": true,
-            "s": "<h3>Тема: Съединителна тъкан</h3><p>Изградена е от голямо количество междуклетъчно вещество и клетки...</p>",
+            "s": "PGgzPlTDgZth0L3QuDog0...0KHRitC10LTQuNC90LjRgtC10LvQvdCwINGC0YrQutCw0L08L2gzPjxwPlHRitC10LTQuNC90LjRgtC10LvQvdCw0YLQsCDRgtGK0LxQsNC9ICg8aT50ZXh0dXMgY29uanVuY3RpdnVzPC9pPikgcNGA0L7QuNC30LvQuNC30LAg0L7RgiDQvNC10LfQtdC90YXQuNC80LAuLi48L3A+",
             "o": [
-                { "x": "1. Хрущялна тъкан", "c": false, "r": "Грешно. Хрущялната тъкан е основна разновидност." },
-                { "x": "2. Костна тъкан", "c": false, "r": "Грешно. Костната тъкан е твърда съединителна тъкан." },
-                { "x": "3. Епителна тъкан", "c": true, "r": "Вярно! Епителната тъкан е самостоятелен вид тъкан." },
-                { "x": "4. Мастна тъкан", "c": false, "r": "Грешно. Мастната тъкан е специализирана форма." }
+                { "x": "6aqf6qyC6qy9uNu/tMOn++Gf7NuMAA==", "c": false, "r": "2KOfIuzM8r4b+A==" },
+                { "x": "6aqf6qyC6qyg0bM75LP8vIu0vM93Wg==", "c": false, "r": "2KOfIuzM8r4b+A==" },
+                { "x": "6aqf6qyC6qyq27M75LP8vIu0vM93Wg==", "c": true, "r": "2aOfIuzM8r4b8g==" },
+                { "x": "6aqf6qyC6qyt0bM75LP8vIu0vM93Wg==", "c": false, "r": "2KOfIuzM8r4b+A==" }
             ]
         }
     ],
