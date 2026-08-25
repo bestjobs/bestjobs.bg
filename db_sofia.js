2b30610f6d3ab5a52b5ef2d5bf23bb9f2100cc76
@bestjobs <bestjobsgithub@gmail.com>
2026-08-25 23:01:02 +0300

    Update db_sofia.js

--- db_sofia.js
+++ db_sofia.js
@@ -1,10 +1,10 @@
 var quizDatabase = {
     "biology": [
         {
-            "salt": "YVhaU2RYbThqYTV0M3M5OQ==",
+            "salt": "ZWM0YTI1YmYwYTFlNDg1Yg==",
             "iv": "bXk1aDdoOWs4ajY1",
-            "t": "9O7EbyNUnVf8V7Uj77U76t9GgP/R0QnI5Xg8V9U=",
-            "q": "r7c8j9o6f5d4k3l2s4e5r6t7u8i9o8z4m9k8j7h6g5f4d3s2a5r6t7u8i9o8zA==",
+            "t": "YzhqOWg2ZjVkNGszbDJzNA==",
+            "q": "bTk4ajdoNmc1ZjRkM3MyYTVyNnQ3dThpOW84ejRtOWs4ajdoNmc1ZjRkM3MyYTVyNnQ3dThpOW84",
             "k": true,
             "s": "PGgzPlTDgZth0L3QuDog0...0KHRitC10LTQuNC90LjRgtC10LvQvdCwINGC0YrQutCw0L08L2gzPjxwPlHRitC10LTQuNC90LjRgtC10LvQvdCw0YLQsCDRgtGK0LrQsNC9ICg8aT50ZXh0dXMgY29uanVuY3RpdnVzPC9pPikgcNGA0L7QuNC30LvQuNC30LAg0L7RgiDQvNC10LfQtdC90YXQuNC80LAuLi48L3A+",
             "o": [
@@ -15,24 +15,24 @@ var quizDatabase = {
             ]
         },
         {
-            "salt": "YVhaU2RYbThqYTV0M3M5OQ==",
+            "salt": "ZWM0YTI1YmYwYTFlNDg1Yg==",
             "iv": "bXk1aDdoOWs4ajY1",
-            "t": "9O7EbyNUnVf8V7Uj77U76t9GgP/R0QnI5Xg8V9U=",
-            "q": "r7c8j9o6f5d4k3l2s4e5r6t7u8i9o8z4m9k8j7h6g5f4d3s2a5r6t7u8i9o8zA==",
+            "t": "YzhqOWg2ZjVkNGszbDJzNA==",
+            "q": "bTk4ajdoNmc1ZjRkM3MyYTVyNnQ3dThpOW84ejRtOWs4ajdoNmc1ZjRkM3MyYTVyNnQ3dThpOW84",
             "k": true,
             "s": "PGgzPlTDgZth0L3QuDog0JLRitCz0LvQtdGF0LjQtNGA0LDRgtC4PC9oMz48cD7QktGK0LPQu9C10YXQuNC00YDQscGC0LjRgtC1INGB0LAg0L7RgdC90L7QstC10L0g0LXQvdC10YDQsy4uLjwvcD+",
             "o": [
                 { "x": "MS4g0LvQuNC/0LjQtNC4", "c": false, "r": "0JPRgNC10YjQvdC+LiDQm9C40L/QuNC00LjRgtC1INGB0LAg0L7RgdC90L7QstC10L0g0L3QsNGA0LDRgcGC0LLQsNGJINC10L3QtdGA0LMu0YDQtdC30LXRgNCyLg==" },
                 { "x": "Mi4g0LLRitCz0bI00YXQuNC00YDQscGC0Lg=", "c": true, "r": "0LLRj9GA0L3QviEg0KLQtdC1INGB0L7RgdC90L7QstC10L0g0LjQt9GC0L7Rh9C90LjQuiDQvdCwINCx0YrRgNC30LAg0LXQvdC10YDQsy4=" },
                 { "x": "My4g0L/RgNC+0YLQtdC40L3QuA==", "c": false, "r": "0JPRgNC10YjQvdC+LiDQn9GA0L7RgtC10LjQvdC40YLQtSDQuNC80LDRgiDQv9GA0LXQtNC40LzQvdC+INGB0YLRgNGD0LrRgtGD0YDQvdCwINGE0YPQvdC60YbQuNGPLg==" },
-                { "x": "NC4g0L3R0LrQu9C10LjQvdC+0LLQuCDQutC40YHQtdC70LjQvdC4", "c": false, "r": "0JPRgNC10YjQvdC+LiDQotC1INGB0YrRhdGA0LDQvdGP0LLQsNGCINCz0LXQvdC10YLQuNGH0L3QsNGC0LAg0LjQvdGE0L7RgNC80LDRhtC40Y8u" }
+                { "x": "NC4g0L3R0LrQu9C10LjQvdC+0LLQuCDQutC40YHQtdC70LjQvdC4", "c": false, "r": "0JPRgNC10YjQvdC+LiDQotC1INGB0Yr|0ZhdGA0LDQvdGP0LLQsNGCINCz0LXQvdC10YLQuNGH0L3QsNGC0LAg0LjQvdGE0L7RgNC80LDRhtC40Y8u" }
             ]
         },
         {
-            "salt": "YVhaU2RYbThqYTV0M3M5OQ==",
+            "salt": "ZWM0YTI1YmYwYTFlNDg1Yg==",
             "iv": "bXk1aDdoOWs4ajY1",
-            "t": "9O7EbyNUnVf8V7Uj77U76t9GgP/R0QnI5Xg8V9U=",
-            "q": "r7c8j9o6f5d4k3l2s4e5r6t7u8i9o8z4m9k8j7h6g5f4d3s2a5r6t7u8i9o8zA==",
+            "t": "YzhqOWg2ZjVkNGszbDJzNA==",
+            "q": "bTk4ajdoNmc1ZjRkM3MyYTVyNnQ3dThpOW84ejRtOWs4ajdoNmc1ZjRkM3MyYTVyNnQ3dThpOW84",
             "k": true,
             "s": "PGgzPlTDgZth0L3QuDog0JzQuNGC0L7RhdC+0L3QtNGA0LjQuDwvaDM+PHA+0JzQuNGC0L7RhdC+0L3QtNGA0LjQuNGC0LUg0YHQsCDQtNCy0YPQvNC10LzQsdGA0LDQvdC90LguLi48L3A+",
             "o": [
