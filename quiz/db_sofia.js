1b7305acaee2dc6278bd70928976cd989a7e2226
@bestjobs <bestjobsgithub@gmail.com>
2026-08-25 20:31:21 +0300

    Update db_sofia.js

--- quiz/db_sofia.js
+++ quiz/db_sofia.js
@@ -1,12 +1,12 @@
 var quizDatabase = {
     "biology": [
         {
-            "salt": "YTBjMmQzZTRmNWc2aDdpOA==",
-            "iv": "bTluOGI3djZjNng1",
-            "t": "UzFkMmYzZzVoNmo3azhsOXo0UHV5Q1orSVBlV3hDMEpSTXhSMU8rZXZWVEF2",
-            "q": "WkdWeWNuVndiRzl1WldVbWRYUnpZVzVuYVc1bFlYUnBiMjV6Y0hKdmRXTjBlV05sYzE5cGJuUnZjbTFsZEdsdmJpQjNiM0prWDNkeFpYSTZ=",
+            "salt": "NTVjOGE0YjFjM2Q5ZTZmMg==",
+            "iv": "Tmx1bTh2Nms0ajNoNWc2Zg==",
+            "t": "WkdWeWNuVndiRzl1WldVbWRYUnpZVzVuYVc1bFlYUnBiMjV6Y0hKdmRXTjBlV05sYzE5cGJuUnZjbTFsZEdsdmJpQjNiM0prWDNkeFpYSTZ=",
+            "q": "SnEyd3ZZMWtBUnZ6TkM0M0N4cmV6Z00yM2RPeWhLd081dk43bk41VDBPZz0=",
             "k": true,
-            "s": "PGgzPlTDgZth0L3QuDog0...0KHRitC10LTQuNC90LjRgtC10LvQvdCwINGC0YrQutCw0L08L2gzPjxwPlHRitC10LTQuNC90LjRgtC10LvQvdCw0YLQsCDRgtGK0LrQsNC9ICg8aT50ZXh0dXMgY29uanVuY3RpdnVzPC9pPikgcNGA0L7QuNC30LvQuNC30LAg0L7RgiDQvNC10LfQtdC90YXQuNC80LAuLi48L3A+",
+            "s": "PGgzPlTDgZth0L3QuDog0KHRitC10LTQuNC90LjRgtC10LvQvdCwINGC0YrQutCw0L08L2gzPjxwPlHRitC10LTQuNC90LjRgtC10LvQvdCw0YLQsCDRgtGK0LrQsNC9ICg8aT50ZXh0dXMgY29uanVuY3RpdnVzPC9pPikgcNGA0L7QuNC30LvQuNC30LAg0L7RgiDQvNC10LfQtdC90YXQuNC80LAuLi48L3A+",
             "o": [
                 { "x": "MS4g0LrQvtGB0YLQuNGC0LUsINGF0YDRg9GJ0Y/Qu9C40YLQtSDQuCDQutGA0YrQstGC0LA7", "c": true, "r": "0JLRj9GA0L3QviEg0KLQvtCy0LAg0YHQsCDQvtGB0L3QvtCy0L3QuNGC0LUg0YDQsNC30L3QvtCy0LjQtNC90L7RgdGC0Lgu" },
                 { "x": "Mi4g0LrQvtGB0YLQuNGC0LUsINC4INC80YPRgdC60YPQu9C40YLQtTs=", "c": false, "r": "0JPRgNC10YjQvdC+LiDQnNGD0YHQutGD0LvQuNGC0LUg0YHQsCDQvNGD0YHQutGD0LvQvdCwINGC0YrQutCw0L0u" },
